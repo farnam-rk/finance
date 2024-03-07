7490e42ed9678af4557f3d1c7e90daf5ddc1bd2c
f.rajabi <farnam.rajabikoochi@gmail.com>
2024-03-07 13:56:41 +0330

    server updated

--- server/server.go
+++ server/server.go
@@ -44,11 +44,6 @@ func Run() {
 
 }
 
-// LoadThread Load All Needed Thread
-func LoadThread() {
-
-}
-
 // CreateAccessFile create accesslog file
 func CreateAccessFile(mypath, format string) *dailyrotate.File {
 
